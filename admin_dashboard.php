222343a84810298f363687903445de611d1e7f64
ghsnts <gustavo4.santos@alunos.ifsuldeminas.edu.br>
2026-05-26 11:23:17 -0300

    Add favicon link to admin dashboard

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -5,6 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Admin Dashboard | Atomicamente</title>
+  <link rel="icon" href="assets/favicon.ico" type="image/x-icon" />
   <link rel="stylesheet" href="css/plataforma.css">
 </head>
 <body class="dash-body">
