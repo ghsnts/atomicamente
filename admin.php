0b025839e48f29ef958ad0e3e29d732bcdbfd2c6
ghsnts <gustavo4.santos@alunos.ifsuldeminas.edu.br>
2026-05-26 11:22:38 -0300

    Add favicon link in admin.php
    Add favicon link to admin page

--- admin.php
+++ admin.php
@@ -12,6 +12,7 @@ session_start();
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Admin | Atomicamente</title>
+  <link rel="icon" href="assets/favicon.ico" type="image/x-icon" />
   <link rel="icon" href="assets/favicon.ico" />
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="css/plataforma.css" />
